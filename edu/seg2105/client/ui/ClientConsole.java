1a25aa9417f9eea3260e6cae9fed4e5b0b7f0738
mfawaz092 <mfawazph@gmail.com>
2023-10-30 20:30:52 -0400

    e1 client b)

--- edu/seg2105/client/ui/ClientConsole.java
+++ edu/seg2105/client/ui/ClientConsole.java
@@ -118,7 +118,7 @@ public class ClientConsole implements ChatIF
   public static void main(String[] args) 
   {
     String host = "";
-
+    int port;
 
     try
     {
@@ -128,7 +128,18 @@ public class ClientConsole implements ChatIF
     {
       host = "localhost";
     }
-    ClientConsole chat= new ClientConsole(host, DEFAULT_PORT);
+    
+    
+    try
+    {
+    	port = Integer.parseInt(args[1]);
+    }
+    catch(Exception e) 
+    {
+    	port = DEFAULT_PORT;
+    }
+    
+    ClientConsole chat= new ClientConsole(host, port);
     chat.accept();  //Wait for console data
   }
 }
